10f260f0c051a071169d500c25885c5c387bf673
Ricky Reusser <rsreusser@gmail.com>
2019-03-25 10:27:33 -0700

    Try UMD

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,35 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import browserifyPlugin from 'rollup-plugin-browserify-transform'
 import es2020 from 'es2020'
 
-export default [{
-  input: 'index.js',
-  output: 'dist/state.js',
-}, {
-  input: 'gui.js',
-  output: 'dist/gui.js',
-}].map(bundle => ({
+export default [
+  {
+    input: 'index.js',
+    output: 'dist/state.js',
+    format: 'cjs',
+  },
+  {
+    input: 'gui.js',
+    output: 'dist/gui.js',
+    format: 'cjs',
+  },
+  {
+    input: 'index.js',
+    output: 'dist/state.umd.js',
+    format: 'umd',
+    name: 'ControlPanelState',
+  },
+  {
+    input: 'gui.js',
+    output: 'dist/gui.umd.js',
+    format: 'umd',
+    name: 'ControlPanelGUI',
+  },
+].map(bundle => ({
   input: bundle.input,
   output: {
     file: bundle.output,
-    format: 'cjs'
+    format: bundle.format,
+    name: bundle.name,
   },
   plugins: [
     nodeResolve({
